dda2a6cecae4604c740e02bb861eb20607633c18
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2023-01-15 23:02:09 +0000

    a

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let buttons = document.getElementsByTagName('button');
+let content = document.getElementById('content')
+let equation = ''
 
 Object.values(buttons).forEach((element) => {
     element.addEventListener('click', function (event) {
@@ -13,24 +15,23 @@ Object.values(buttons).forEach((element) => {
 window.addEventListener('keydown', function(event) {
     event.preventDefault()
 
-    switch(event.key) {
-        case '1':
-            console.log(event.key)
-            break
-        case 'ArrowDown':
-            console.log(1)
-            break
-        case 'ArrowDown':
-            console.log(1)
-            break
-        case 'ArrowDown':
-            console.log(1)
-            break 
-        case 'ArrowDown':
-            console.log(1)
-            break
-        default:
-            console.log(event.key)
-            return
-    }
+    if (['+', '-', 'x', '/'].find((element) => element == event.key)) {
+        content.innerText = 'THISSHOULDBEINVISIBLE'
+
+        equation += event.key
+        if (content.innerText === 0) {
+        }
+    } else if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'].find((element) => element == event.key)){
+        console.log('hi')
+        if (triggedOnce === false) {
+            equation += event.key
+            content.innerText = equation
+            triggedOnce = true
+        } else {
+            equation += event.key
+            content.innerText += event.key
+        }
+    } else [
+        console.log(2)
+    ]
 })
