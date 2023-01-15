7bfa2675a67fe1f434cd1873d854022add22986d
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2023-01-15 19:35:54 +0000

    a

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-
 let buttons = document.getElementsByTagName('button');
 
-function onClick(element) {
-    let id = element.id
-    let classList = element.classList
-
-    console.log(1)
-}
-
 Object.values(buttons).forEach((element) => {
-    element.addEventListener('click', () => onClick(element))
+    element.addEventListener('click', function (event) {
+        event.preventDefault()
+        let id = element.id
+        let classList = element.classList
+
+        console.log(1)
+    })
+})
+
+window.addEventListener('keydown', function(event) {
+    event.preventDefault()
+
+    switch(event.key) {
+        case '1':
+            console.log(event.key)
+            break
+        case 'ArrowDown':
+            console.log(1)
+            break
+        case 'ArrowDown':
+            console.log(1)
+            break
+        case 'ArrowDown':
+            console.log(1)
+            break 
+        case 'ArrowDown':
+            console.log(1)
+            break
+        default:
+            console.log(event.key)
+            return
+    }
 })
